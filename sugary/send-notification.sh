44f505b3f03d7faabf3126bd7a5448257fbd0b08
renantrendt <159676672+renantrendt@users.noreply.github.com>
2025-12-31 16:50:29 -0800

    ,

--- sugary/send-notification.sh
+++ sugary/send-notification.sh
@@ -1,4 +1,10 @@
 #this file is to send custom push notification to all subscribers
+
+###copy this code and paste on terminal
+###cd ~/CascadeProjects/Sugary/sugary
+###./send-notification.sh
+
+
 #!/bin/bash
 
 # Send Push Notification to all Sugary subscribers
@@ -7,7 +13,7 @@
 SUPABASE_URL="https://ioyixenqdshxqgqoocwj.supabase.co/functions/v1/send-weekly-ranking"
 SERVICE_ROLE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlveWl4ZW5xZHNoeHFncW9vY3dqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NzIwNDcyNywiZXhwIjoyMDgyNzgwNzI3fQ.XqJSKaxAzaY5zAfSL5LH9C4mLgqYgUHtx2z5DyXQ97A"
 
-echo "YOUR MESSAGE HERE"
+echo "Testing push notification"
 echo ""
 
 curl -X POST "$SUPABASE_URL" \
